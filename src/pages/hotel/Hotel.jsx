829299e8c8e1813264ae86d2615f3cb20383e4db
Neil <neilrowland56@yahoo.com>
2022-05-25 14:08:44 +0100

    Modal added to Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import MailList from '../../components/mailList/MailList';
+import Footer from '../../components/footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faCircleArrowLeft,
@@ -10,6 +12,9 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 function Hotel() {
+	const [slideNumber, setSlideNumber] = useState(0);
+	const [open, setOpen] = useState(false);
+
 	const photos = [
 		{
 			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1',
@@ -31,12 +36,57 @@ function Hotel() {
 		},
 	];
 
+	const handleOpen = (index) => {
+		setSlideNumber(index);
+
+		setOpen(true);
+	};
+
+	const handleMove = (direction) => {
+		let newSlideNumber;
+
+		if (direction === 'l') {
+			newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+		} else {
+			newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+		}
+
+		setSlideNumber(newSlideNumber);
+	};
+
 	return (
 		<>
 			<Navbar />
 			<Header type="list" />
 			<div className="flex flex-col w-full max-w-[1024px] mx-auto">
-				<div className="flex w-full justify-between max-w-[1024px] mx-auto mt-8 ">
+				{open && (
+					<div className="sticky top-0 left-0 w-full h-screen bg-black object-fit overflow-hidden z-40  pt-10 opacity-90">
+						<FontAwesomeIcon
+							icon={faCircleXmark}
+							className="text-white absolute top-1 right-5 h-8 w-8"
+							onClick={() => setOpen(false)}
+						/>
+
+						<div className=" flex flex-col   relative ">
+							<FontAwesomeIcon
+								icon={faCircleArrowLeft}
+								className="text-white absolute top-1/2 left-10 w-12 h-12 cursor-pointer "
+								onClick={() => handleMove('l')}
+							/>
+							<img
+								src={photos[slideNumber].src}
+								alt=""
+								className="sliderImg"
+							/>
+							<FontAwesomeIcon
+								icon={faCircleArrowRight}
+								className="text-white absolute top-1/2 right-10 w-12 h-12 cursor-pointer"
+								onClick={() => handleMove('r')}
+							/>
+						</div>
+					</div>
+				)}
+				<div className="flex w-full justify-between max-w-[1024px] mx-auto mt-8 -z-10">
 					<h1 className="font-bold">Tower Street Apartments</h1>
 					<button className="  cursor-pointer text-[#fff] bg-[#0071c2] rounded  px-1 h-16  md:h-8 my-2 font-semibold   ">
 						Reserve or Book Now!
@@ -59,9 +109,14 @@ function Hotel() {
 					</span>
 				</div>
 				<div className="grid grid-cols-3 gap-4 max-w-[1024px] mx-auto ">
-					{photos.map((photo, i) => (
-						<div className="" key={i}>
-							<img src={photo.src} alt="" className="" />
+					{photos.map((photo, index) => (
+						<div className="" key={index}>
+							<img
+								onClick={() => handleOpen(index)}
+								src={photo.src}
+								alt=""
+								className=""
+							/>
 						</div>
 					))}
 				</div>
@@ -102,6 +157,8 @@ function Hotel() {
 						</button>
 					</div>
 				</div>
+				<MailList />
+				<Footer />
 			</div>
 		</>
 	);
