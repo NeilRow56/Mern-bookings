6e775bb312677af74b3a110898ee3ee46c0614cf
Neil <neilrowland56@yahoo.com>
2022-05-31 08:01:04 +0100

    updated client/pages/hotel/Hotel.jsx for database info

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const SearchItem = () => {
+const SearchItem = ({ item }) => {
 	return (
 		<div className="grid grid-cols-5 p-2  gap-4 mb-4 border border-gray-200 rounded-md">
 			<div className="w-[170px] h-[170px] col-span-1 object-cover">
-				<img
-					src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1"
-					alt=""
-				/>
+				<img src={item.photos[0]} alt="" />
 			</div>
 			<div className="col-span-3 w-full flex flex-col ml-3">
-				<h3 className="text-[#0071c2] font-bold ">
-					Tower Street Apartments
-				</h3>
-				<span className="text-sm">500m from center</span>
+				<h3 className="text-[#0071c2] font-bold ">{item.name}</h3>
+				<span className="text-sm">{item.distance}m from center</span>
 				<span className="bg-[#008009] rounded-lg w-[150px] text-[#fff] px-1 my-1">
 					Free airport taxi
 				</span>
 				<span className="text-sm font-semibold mb-1">
 					Studio Apartment with Air conditioning
 				</span>
-				<span className="text-sm mb-1">
-					Entire studio • 1 bathroom • 21m² 1 full bed
-				</span>
+				<span className="text-sm mb-1">{item.desc}</span>
 				<span className="text-sm font-bold text-[#008009] mb-1">
 					Free cancellation{' '}
 				</span>
@@ -31,19 +25,25 @@ const SearchItem = () => {
 				</span>
 			</div>
 			<div className="w-full">
-				<div className="flex justify-between">
-					<span>Excellent</span>
-					<button className="bg-[#003580] text-white rounded mr-1 px-1 border border-gray-800">
-						8.9
-					</button>
-				</div>
+				{item.rating && (
+					<div className="flex justify-between">
+						<span>Excellent</span>
+						<button className="bg-[#003580] text-white rounded mr-1 px-1 border border-gray-800">
+							{item.rating}
+						</button>
+					</div>
+				)}
 				<div className="flex flex-col mt-5">
-					<span className="font-bold text-[20px]">£112</span>
+					<span className="font-bold text-[20px]">
+						£{item.cheapestPrice}
+					</span>
 					<span className="text-sm">Includes taxes and fees</span>
 					<div className="w-80%  mt-6 mr-2 ">
-						<button className="  cursor-pointer text-[#fff] bg-[#0071c2] rounded  px-1  h-8 mb-2 font-semibold w-full mx-2 ">
-							See availability
-						</button>
+						<Link to={`/hotels/${item._id}`}>
+							<button className="  cursor-pointer text-[#fff] bg-[#0071c2] rounded  px-1  h-8 mb-2 font-semibold w-full mx-2 ">
+								See availability
+							</button>
+						</Link>
 					</div>
 				</div>
 			</div>
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import useFetch from '../../hooks/useFetch';
+import { useLocation } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+
 import MailList from '../../components/mailList/MailList';
 import Footer from '../../components/footer/Footer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,29 +15,14 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 function Hotel() {
+	const location = useLocation();
+	const id = location.pathname.split('/')[2];
+
 	const [slideNumber, setSlideNumber] = useState(0);
 	const [open, setOpen] = useState(false);
-
-	const photos = [
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1',
-		},
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1',
-		},
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1',
-		},
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1',
-		},
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1',
-		},
-		{
-			src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1',
-		},
-	];
+	const { data, loading, error } = useFetch(
+		`http://localhost:8800/api/hotels/find/${id}`
+	);
 
 	const handleOpen = (index) => {
 		setSlideNumber(index);
@@ -58,108 +46,96 @@ function Hotel() {
 		<>
 			<Navbar />
 			<Header type="list" />
-			<div className="flex flex-col w-full max-w-[1024px] mx-auto">
-				{open && (
-					<div className="sticky top-0 left-0 w-full h-screen bg-black object-fit overflow-hidden z-40  pt-10 opacity-90">
-						<FontAwesomeIcon
-							icon={faCircleXmark}
-							className="text-white absolute top-1 right-5 h-8 w-8"
-							onClick={() => setOpen(false)}
-						/>
+			{loading ? (
+				'loading...'
+			) : (
+				<div className="flex flex-col w-full max-w-[1024px] mx-auto">
+					{open && (
+						<div className="sticky top-0 left-0 w-full h-screen bg-black object-fit overflow-hidden z-40  pt-10 opacity-90">
+							<FontAwesomeIcon
+								icon={faCircleXmark}
+								className="text-white absolute top-1 right-5 h-8 w-8"
+								onClick={() => setOpen(false)}
+							/>
 
-						<div className=" flex flex-col   relative ">
-							<FontAwesomeIcon
-								icon={faCircleArrowLeft}
-								className="text-white absolute top-1/2 left-10 w-12 h-12 cursor-pointer "
-								onClick={() => handleMove('l')}
-							/>
-							<img
-								src={photos[slideNumber].src}
-								alt=""
-								className="sliderImg"
-							/>
-							<FontAwesomeIcon
-								icon={faCircleArrowRight}
-								className="text-white absolute top-1/2 right-10 w-12 h-12 cursor-pointer"
-								onClick={() => handleMove('r')}
-							/>
+							<div className=" flex flex-col   relative ">
+								<FontAwesomeIcon
+									icon={faCircleArrowLeft}
+									className="text-white absolute top-1/2 left-10 w-12 h-12 cursor-pointer "
+									onClick={() => handleMove('l')}
+								/>
+								<img
+									src={data.photos[slideNumber]}
+									alt=""
+									className="sliderImg"
+								/>
+								<FontAwesomeIcon
+									icon={faCircleArrowRight}
+									className="text-white absolute top-1/2 right-10 w-12 h-12 cursor-pointer"
+									onClick={() => handleMove('r')}
+								/>
+							</div>
 						</div>
-					</div>
-				)}
-				<div className="flex w-full justify-between max-w-[1024px] mx-auto mt-8 -z-10">
-					<h1 className="font-bold">Tower Street Apartments</h1>
-					<button className="  cursor-pointer text-[#fff] bg-[#0071c2] rounded  px-1 h-16  md:h-8 my-2 font-semibold   ">
-						Reserve or Book Now!
-					</button>
-				</div>
-				<div className="w-full max-w-[1024px] mx-auto flex flex-col">
-					<div className="">
-						<FontAwesomeIcon
-							className="mr-4"
-							icon={faLocationDot}
-						/>
-						<span className="text-sm">Elton St 125 New york</span>
-					</div>
-					<span className="text-[#0071c2] font-bold">
-						Excellent location – 500m from center
-					</span>
-					<span className="text-[#008009] my-2 font-bold">
-						Book a stay over £114 at this property and get a free
-						airport taxi
-					</span>
-				</div>
-				<div className="grid grid-cols-3 gap-4 max-w-[1024px] mx-auto ">
-					{photos.map((photo, index) => (
-						<div className="" key={index}>
-							<img
-								onClick={() => handleOpen(index)}
-								src={photo.src}
-								alt=""
-								className=""
-							/>
-						</div>
-					))}
-				</div>
-				<div className="grid grid-cols-8 mb-5">
-					<div className="col-span-6 w-full flex flex-col px-3">
-						<h1 className="font-bold my-8">
-							Stay in the heart of City
-						</h1>
-						<p className="hotelDesc">
-							Located a 5-minute walk from St. Florian's Gate in
-							Krakow, Tower Street Apartments has accommodations
-							with air conditioning and free WiFi. The units come
-							with hardwood floors and feature a fully equipped
-							kitchenette with a microwave, a flat-screen TV, and
-							a private bathroom with shower and a hairdryer. A
-							fridge is also offered, as well as an electric tea
-							pot and a coffee machine. Popular points of interest
-							near the apartment include Cloth Hall, Main Market
-							Square and Town Hall Tower. The nearest airport is
-							John Paul II International Kraków–Balice, 16.1 km
-							from Tower Street Apartments, and the property
-							offers a paid airport shuttle service.
-						</p>
-					</div>
-					<div className="col-span-2 w-full flex flex-col mt-8 ml-3 bg-[#ebf3ff]  rounded-md p-2">
-						<h4 className="font-bold text-[18px]">
-							Perfect for a 9-night stay!
-						</h4>
-						<span className="my-4">
-							Located in the real heart of Krakow, this property
-							has an excellent location score of 9.8!
-						</span>
-						<h2 className="mb-7">
-							<b>£945</b> (9 nights)
-						</h2>
-						<button className="bg-[#0071c2] h-16 px-1 rounded-md text-[#fff] md:h-8 items-center mx-3">
+					)}
+					<div className="flex w-full justify-between max-w-[1024px] mx-auto mt-8 -z-10">
+						<h1 className="font-bold">{data.name}</h1>
+						<button className="  cursor-pointer text-[#fff] bg-[#0071c2] rounded  px-1 h-16  md:h-8 my-2 font-semibold   ">
 							Reserve or Book Now!
 						</button>
 					</div>
+					<div className="w-full max-w-[1024px] mx-auto flex flex-col">
+						<div className="">
+							<FontAwesomeIcon
+								className="mr-4"
+								icon={faLocationDot}
+							/>
+							<span className="text-sm">{data.address}</span>
+						</div>
+						<span className="text-[#0071c2] font-bold">
+							Excellent location – {data.distance}m from center
+						</span>
+						<span className="text-[#008009] my-2 font-bold">
+							Book a stay over £{data.cheapestPrice} at this
+							property and get a free airport taxi
+						</span>
+					</div>
+					<div className="grid grid-cols-3 gap-4 max-w-[1024px] mx-auto ">
+						{data.photos?.map((photo, index) => (
+							<div className="" key={index}>
+								<img
+									onClick={() => handleOpen(index)}
+									src={photo}
+									alt=""
+									className=""
+								/>
+							</div>
+						))}
+					</div>
+					<div className="grid grid-cols-8 mb-5">
+						<div className="col-span-6 w-full flex flex-col px-3">
+							<h1 className="font-bold my-8">{data.title}</h1>
+							<p className="hotelDesc">{data.desc}</p>
+						</div>
+						<div className="col-span-2 w-full flex flex-col mt-8 ml-3 bg-[#ebf3ff]  rounded-md p-2">
+							<h4 className="font-bold text-[18px]">
+								Perfect for a 9-night stay!
+							</h4>
+							<span className="my-4">
+								Located in the real heart of Krakow, this
+								property has an excellent location score of 9.8!
+							</span>
+							<h2 className="mb-7">
+								<b>£945</b> (9 nights)
+							</h2>
+							<button className="bg-[#0071c2] h-16 px-1 rounded-md text-[#fff] md:h-8 items-center mx-3">
+								Reserve or Book Now!
+							</button>
+						</div>
+					</div>
+					<MailList />
+					<Footer />
 				</div>
-				<MailList />
-				<Footer />
-			</div>
+			)}
 		</>
 	);
 }
